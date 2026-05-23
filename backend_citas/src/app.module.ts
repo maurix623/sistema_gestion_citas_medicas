8909aa0cf7c080aea88c655908a8a1a1553bda7d
maurix623 <maugithub2012@gmail.com>
2026-05-22 21:27:45 -0400

    conexion postgresql con typeorm

--- backend_citas/src/app.module.ts
+++ backend_citas/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host : 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '9923343',
+      database: 'bd-tienda-online',
+      autoLoadEntities: true, // Carga automáticamente las entidades
+      synchronize: true, // Sincroniza la base de datos con las entidades 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

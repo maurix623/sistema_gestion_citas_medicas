38ab32fa6ea4e32da8b74d067d554b4346c4de37
maurix623 <maugithub2012@gmail.com>
2026-05-30 21:18:23 -0400

    auth implemetacion autorizacion por roles con guards y decoradores, ademas de jwtStrategy para validacion de JWT

--- backend_citas/src/auth/auth.controller.ts
+++ backend_citas/src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guards';
+import { RolesGuard } from './guards/roles.guard';
+import { Roles } from './decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +14,12 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto,){
         return this.authService.login(loginDto,);
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('ADMIN')
+    @Get('perfil')
+    getPerfil(@Request() req){
+        return req.user; 
     }
 }
--- backend_citas/src/auth/auth.module.ts
+++ backend_citas/src/auth/auth.module.ts
@@ -4,14 +4,15 @@ import { AuthController } from './auth.controller';
 import { UsuarioModule } from 'src/usuario/usuario.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 
 @Module({
   imports: [UsuarioModule, PassportModule, JwtModule.register({
-    secret: 'mi_clave_secreta',
+    secret: 'yo123',
     signOptions:{expiresIn: '1h'},
   })],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
